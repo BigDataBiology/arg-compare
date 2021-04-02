fa7f437569235a8497df53e25febc57407df8f1a
Luis Pedro Coelho <luis@luispedro.org>
2021-04-02 08:53:53 +0000

    added fna support

--- jugfile.py
+++ jugfile.py
@@ -5,19 +5,24 @@ import itertools
 MAX_SEQ_CHUNKS = 1000
 
 @TaskGenerator
-def split_seq_file(faa):
+def split_seq_file(fa):
     from os import makedirs, path
     from fasta import fasta_iter
     import gzip
     makedirs('partials', exist_ok=True)
-    basename = path.basename(faa)
+    basename = path.basename(fa)
     cur_n = MAX_SEQ_CHUNKS + 1
     ix = 0
     out = None
     partials = []
-    for h, seq in fasta_iter(faa):
+    for h, seq in fasta_iter(fa):
         if cur_n >= MAX_SEQ_CHUNKS:
-            partials.append(f'partials/{basename}_block_{ix:04}.faa.gz')
+            if fa.endswith('.faa.gz'):
+                partials.append(f'partials/{basename}_block_{ix:04}.faa.gz')
+            elif fa.endswith('.fna.gz'):
+                partials.append(f'partials/{basename}_block_{ix:04}.fna.gz')
+            else:
+                raise ValueError(f'Unexpected file name: {fa}')
             out = gzip.open(partials[-1], compresslevel = 1, mode = 'wt')
             cur_n = 0
             ix += 1
@@ -64,11 +69,13 @@ def concat_partials(partials, oname):
     full.to_csv(oname, sep='\t')
     return oname
 
-   
-splits = split_seq_file('data/GMGC10.wastewater.95nr.faa.gz')
-partials = []
-for faa in (bvalue(splits)):
-    partials.append(run_rgi(faa))
 
+splits_faa = split_seq_file('data/GMGC10.wastewater.95nr.test_10k.faa.gz')
+partials = []
+for faa in (bvalue(splits_faa)):
+    partials.append(run_rgi(faa))
 concat_partials(partials, 'outputs/rgi.full.tsv.gz')
+
+splits_fna = split_seq_file('data/GMGC10.wastewater.95nr.test_10k.fna.gz')
+
 
