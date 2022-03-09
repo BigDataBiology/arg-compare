3fdb5853df574e4c905fff5e00e8def5d7e6c324
cocodyq <55238881+cocodyq@users.noreply.github.com>
2022-03-09 23:12:29 +0800

    Add ARGs-OAP results and codes

---
+++ ARGs-OAP/run_ARGsOAP.sh
@@ -0,0 +1,1 @@
+singularity exec -B /home/yiqian/AMR/ARG-OAP/inputdir /home/yiqian/AMR/ARG-OAP/argoapv2.5.sif /home/argsoapv2.5/argoap_version2.5 -i /home/yiqian/AMR/ARG-OAP/inputdir -m /home/yiqian/AMR/ARG-OAP/test_meta.txt -o /home/yiqian/AMR/ARG-OAP/test_out -n 8 -z
