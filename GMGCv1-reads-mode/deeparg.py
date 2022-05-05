59730c9e4ed04159fced702db9def868673a4166
Luis Pedro Coelho <luis@luispedro.org>
2022-05-05 14:35:35 +0200

    ENH Compress output files

--- GMGCv1-reads-mode/deeparg.py
+++ GMGCv1-reads-mode/deeparg.py
@@ -50,6 +50,16 @@ def clean_up_deeparg_outputs(basedir):
 
     return basedir
 
+@TaskGenerator
+def xz_files(basedir):
+    print(basedir)
+    from glob import glob
+    import subprocess
+    for f in glob(basedir + '/*'):
+        if not f.endswith('.xz'):
+            subprocess.check_call(['xz', f])
+    return basedir
+
 
 @TaskGenerator
 def build_table(outputs):
@@ -57,7 +67,7 @@ def build_table(outputs):
     data = {}
     for s, basedir in outputs.items():
         data[s] = pd.read_table(
-                    basedir + '/output.deeparg.clean.deeparg.mapping.ARG.merged.quant',
+                    basedir + '/output.deeparg.clean.deeparg.mapping.ARG.merged.quant.xz',
                     names=['Symbol', 'category', 'nrReads', 'B', 'C', 'D']) \
                             .set_index("Symbol")['nrReads']
     data = pd.DataFrame(data).fillna(0).astype(int)
@@ -72,7 +82,7 @@ data_dir = deeparg_get_data(DATA_DIR)
 
 deeparg_outputs = {}
 for s in samples:
-    deeparg_outputs[s] = clean_up_deeparg_outputs(run_sample(data_dir, s))
+    deeparg_outputs[s] = xz_files(clean_up_deeparg_outputs(run_sample(data_dir, s)))
 
 outs = identity(deeparg_outputs)
 build_table(outs)
