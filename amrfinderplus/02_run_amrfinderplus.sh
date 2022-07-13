e6e6815991caf46e389e246121b1128929795acd
cocodyq <55238881+cocodyq@users.noreply.github.com>
2022-07-14 01:13:18 +0800

    fix codes

--- amrfinderplus/02_run_amrfinderplus.sh
+++ amrfinderplus/02_run_amrfinderplus.sh
@@ -6,6 +6,6 @@ if [ "$(ls -A $DIR)" ]
 then
   for infile in $(ls ${DIR})
     do
-	  amrfinder -p ${DIR}/${infile} --threads 64 -o ~/data/result/${infile}.tsv
-	done
+      amrfinder -p ${DIR}/${infile} --threads 64 -o ~/data/result/${infile}.tsv
+    done
 fi
